d6a9c8c425968a2b4ce46a1b7d39ae9791280515
vincseize <vincseize@gmail.com>
2015-12-01 20:13:37 +0100

    Simplify generique c_update.php

--- admin/c_Update.php
+++ admin/c_Update.php
@@ -1,19 +1,21 @@
 <?php
-
 	require_once("../__Globals.php");
 	require_once("__dbcontroller.php");
 	$db_handle = new DBController();
 
 	$sql_table = $_GET['sql_table'] ;
 	$id = $_REQUEST['id'] ;
-	$value = $_REQUEST['value'] ;
+	$value = mysql_real_escape_string(htmlentities($_REQUEST['value'], ENT_QUOTES, "UTF-8"));
 	$columnName = $_REQUEST['columnName'] ;
+/*	$columnPosition = $_REQUEST['columnPosition'] ;
+	$columnId = $_REQUEST['columnId'] ;
+	$rowId = $_REQUEST['rowId'] ;*/
+	
+	// if(isset($value)){ $value = mysql_real_escape_string(htmlentities($value, ENT_QUOTES, "UTF-8")); };
 
-	if(isset($value)){ $value = mysql_real_escape_string(htmlentities($value, ENT_QUOTES, "UTF-8")); };
 	$result = mysql_query("UPDATE ".$sql_table." set ".$columnName." = '".$value."' WHERE  id=".$id);
 
 	echo $_REQUEST['value']; // Important, for not alert !  or id ????
-
 ?>
 
 
