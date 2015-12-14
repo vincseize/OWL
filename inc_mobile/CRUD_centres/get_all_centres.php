c32b1d9f636b531139a2f04c75a2621c01bde33e
vincseize <vincseize@gmail.com>
2015-12-14 19:18:54 +0100

    reformat color_type

--- inc_mobile/CRUD_centres/get_all_centres.php
+++ inc_mobile/CRUD_centres/get_all_centres.php
@@ -11,6 +11,159 @@ $action_key = "83abec5a494b89095d07e031445de903";
 $response = array();
  
 
+function hex2rgb($hex) {
+   $hex = str_replace("#", "", $hex);
+
+   if(strlen($hex) == 3) {
+      $r = hexdec(substr($hex,0,1).substr($hex,0,1));
+      $g = hexdec(substr($hex,1,1).substr($hex,1,1));
+      $b = hexdec(substr($hex,2,1).substr($hex,2,1));
+   } else {
+      $r = hexdec(substr($hex,0,2));
+      $g = hexdec(substr($hex,2,2));
+      $b = hexdec(substr($hex,4,2));
+   }
+   $rgb = array($r, $g, $b);
+   //return implode(",", $rgb); // returns the rgb values separated by commas
+   return $rgb; // returns an array with the rgb values
+}
+
+
+
+function RGB_TO_HSV($R, $G, $B)    // RGB values:    0-255, 0-255, 0-255
+{                                // HSV values:    0-360, 0-100, 0-100
+    // Convert the RGB byte-values to percentages
+    $R = ($R / 255);
+    $G = ($G / 255);
+    $B = ($B / 255);
+
+    // Calculate a few basic values, the maximum value of R,G,B, the
+    //   minimum value, and the difference of the two (chroma).
+    $maxRGB = max($R, $G, $B);
+    $minRGB = min($R, $G, $B);
+    $chroma = $maxRGB - $minRGB;
+
+    // Value (also called Brightness) is the easiest component to calculate,
+    //   and is simply the highest value among the R,G,B components.
+    // We multiply by 100 to turn the decimal into a readable percent value.
+    $computedV = 100 * $maxRGB;
+
+    // Special case if hueless (equal parts RGB make black, white, or grays)
+    // Note that Hue is technically undefined when chroma is zero, as
+    //   attempting to calculate it would cause division by zero (see
+    //   below), so most applications simply substitute a Hue of zero.
+    // Saturation will always be zero in this case, see below for details.
+    if ($chroma == 0)
+        return array(0, 0, $computedV);
+
+    // Saturation is also simple to compute, and is simply the chroma
+    //   over the Value (or Brightness)
+    // Again, multiplied by 100 to get a percentage.
+    $computedS = 100 * ($chroma / $maxRGB);
+
+    // Calculate Hue component
+    // Hue is calculated on the "chromacity plane", which is represented
+    //   as a 2D hexagon, divided into six 60-degree sectors. We calculate
+    //   the bisecting angle as a value 0 <= x < 6, that represents which
+    //   portion of which sector the line falls on.
+    if ($R == $minRGB)
+        $h = 3 - (($G - $B) / $chroma);
+    elseif ($B == $minRGB)
+        $h = 1 - (($R - $G) / $chroma);
+    else // $G == $minRGB
+        $h = 5 - (($B - $R) / $chroma);
+
+    // After we have the sector position, we multiply it by the size of
+    //   each sector's arc (60 degrees) to obtain the angle in degrees.
+    $computedH = 60 * $h;
+
+    return array($computedH, $computedS, $computedV);
+}
+
+
+function HexHSL( $HexColor ){
+
+        $HexColor    = str_replace( '#', '', $HexColor );
+
+        if( strlen( $HexColor ) < 3 ) str_pad( $HexColor, 3 - strlen( $HexColor ), '0' );
+
+        $Add         = strlen( $HexColor ) == 6 ? 2 : 1;
+        $AA          = 0;
+        $AddOn       = $Add == 1 ? ( $AA = 16 - 1 ) + 1 : 1;
+
+        $Red         = round( ( hexdec( substr( $HexColor, 0, $Add ) ) * $AddOn + $AA ) / 255, 6 );
+        $Green       = round( ( hexdec( substr( $HexColor, $Add, $Add ) ) * $AddOn + $AA ) / 255, 6 );
+        $Blue        = round( ( hexdec( substr( $HexColor, ( $Add + $Add ) , $Add ) ) * $AddOn + $AA ) / 255, 6 );
+
+
+        $HSLColor    = array( 'Hue' => 0, 'Saturation' => 0, 'Luminance' => 0 );
+
+        $Minimum     = min( $Red, $Green, $Blue );
+        $Maximum     = max( $Red, $Green, $Blue );
+
+        $Chroma      = $Maximum - $Minimum;
+
+
+        $HSLColor['Luminance'] = ( $Minimum + $Maximum ) / 2;
+
+        if( $Chroma == 0 )
+        {
+                $HSLColor['Luminance'] = round( $HSLColor['Luminance'] * 255, 0 );
+
+                return $HSLColor;
+        }
+
+
+        $Range = $Chroma * 6;
+
+
+        $HSLColor['Saturation'] = $HSLColor['Luminance'] <= 0.5 ? $Chroma / ( $HSLColor['Luminance'] * 2 ) : $Chroma / ( 2 - ( $HSLColor['Luminance'] * 2 ) );
+
+        if( $Red <= 0.004 || $Green <= 0.004 || $Blue <= 0.004 )
+                $HSLColor['Saturation'] = 1;
+
+
+        if( $Maximum == $Red )
+        {
+                $HSLColor['Hue'] = round( ( $Blue > $Green ? 1 - ( abs( $Green - $Blue ) / $Range ) : ( $Green - $Blue ) / $Range ) * 255, 0 );
+        }
+        else if( $Maximum == $Green )
+        {
+                $HSLColor['Hue'] = round( ( $Red > $Blue ? abs( 1 - ( 4 / 3 ) + ( abs ( $Blue - $Red ) / $Range ) ) : ( 1 / 3 ) + ( $Blue - $Red ) / $Range ) * 255, 0 );
+        }
+        else
+        {
+                $HSLColor['Hue'] = round( ( $Green < $Red ? 1 - 2 / 3 + abs( $Red - $Green ) / $Range : 2 / 3 + ( $Red - $Green ) / $Range ) * 255, 0 );
+        }
+
+
+
+
+        $HSLColor['Saturation'] = round( $HSLColor['Saturation'] * 255, 0 );
+        $HSLColor['Luminance']  = round( $HSLColor['Luminance'] * 255, 0 );
+
+
+
+        return $HSLColor;
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 if(isset($_POST['action_key'])==$action_key || isset($_GET['action_key'])==$action_key){
 
     if (isset($_POST['action_key']) == $action_key || isset($_GET['action_key']) == $action_key) {
@@ -176,7 +329,12 @@ foreach($json->people as $item)
                                                                         $get_type = searchJson_type($response_type,$id_type);
                                                                         $product["name_type"] = $get_type[0];
                                                                         $product["description_type"] = $get_type[1];
-                                                                        $product["color_type"] = $get_type[2];
+                                                                        $product["color_type"] = trim(strtolower($get_type[2]));
+                                                                        $product_rgb = hex2rgb($product["color_type"]);
+                                                                        $product["color_type_hsl"] = RGB_TO_HSV($product_rgb[0],$product_rgb[1],$product_rgb[2]);
+                                                                        $product_hsl = RGB_TO_HSV($product_rgb[0],$product_rgb[1],$product_rgb[2]);
+                                                                        $product["color_type_hsl_android"] = $product_hsl[0];
+                                                                        
                                                                        
 
 
